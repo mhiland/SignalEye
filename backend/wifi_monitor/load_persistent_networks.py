4adf99f747eb4976cdbdc546ede10990a5576740
Mike Hiland <mikehiland@gmail.com>
2024-08-04 16:47:09 -0700

    Add Json Schema, tests and validation on json file read/write

--- backend/wifi_monitor/load_persistent_networks.py
+++ backend/wifi_monitor/load_persistent_networks.py
@@ -1,5 +1,9 @@
 import os
 import json
+from jsonschema import validate, ValidationError
+from json_schema import schema
+import logging
+import logging_config
 
 LOG_DIR = os.path.join('/', 'var', 'log', 'wifi_monitor')
 PERSISTENT_FILE = os.path.join(LOG_DIR, 'persistent_networks.json')
@@ -8,10 +12,19 @@ PERSISTENT_FILE = os.path.join(LOG_DIR, 'persistent_networks.json')
 def load_persistent_networks():
     if os.path.exists(PERSISTENT_FILE):
         with open(PERSISTENT_FILE, 'r', encoding='utf-8') as f:
-            return json.load(f)
+            persistent_networks = json.load(f)
+            try:
+                validate(instance=persistent_networks, schema=schema)
+                return persistent_networks
+            except ValidationError as e:
+                logging.error("Validation error loading persistent networks.")
     return []
 
 
 def save_persistent_networks(persistent_networks):
-    with open(PERSISTENT_FILE, 'w', encoding='utf-8') as f:
-        json.dump(persistent_networks, f, indent=4)
+    try:
+        validate(instance=persistent_networks, schema=schema)
+        with open(PERSISTENT_FILE, 'w', encoding='utf-8') as f:
+            json.dump(persistent_networks, f, indent=4)
+    except ValidationError as e:
+        logging.error("Validation error saving persistent networks.")
